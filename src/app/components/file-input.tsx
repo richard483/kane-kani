1f5f5ba1fc47aa8c31903fa53839af7e954dfc3b
richard483 <richard.william483@gmail.com>
2025-10-19 01:18:37 +0700

    fix: improved compression setting & logging

--- src/app/components/file-input.tsx
+++ src/app/components/file-input.tsx
@@ -4,12 +4,16 @@ import FileInputForm from './file-input-form';
 import { BillData, GeminiBillResponse } from '../types/Bill';
 import { getGeminiRequestData } from '../utils/gemini';
 
-async function compressRawBase64String(buffer: Buffer): Promise<string> {
+async function compressBufferToBase64(buffer: Buffer): Promise<string> {
   'use server';
 
   const compressedBuffer = await sharp(buffer)
-    .resize(800)
-    .jpeg({ quality: 50 })
+    .grayscale()
+    .jpeg({
+      quality: 50,
+      mozjpeg: true,
+      overshootDeringing: true,
+    })
     .toBuffer();
   return compressedBuffer.toString('base64');
 }
@@ -29,10 +33,14 @@ async function handleFileProcessing(
   try {
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
-    const compressedBase64String = await compressRawBase64String(buffer);
     console.log(
-      '#handleFileProcessing - Compressed base64 string:',
-      compressedBase64String,
+      '#handleFileProcessing - Pre-compressed buffer length:',
+      buffer.toString('base64').length,
+    );
+    const compressedBase64String = await compressBufferToBase64(buffer);
+    console.log(
+      '#handleFileProcessing - Compressed buffer length:',
+      compressedBase64String.length,
     );
 
     const response = await axios(
